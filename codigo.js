6e2b9de24e4efb5673b1859422bc808b8fb5a4d7
Andercmdsena <andercmdsena>
2023-10-23 09:52:19 -0500

    hola

--- codigo.js
+++ codigo.js
@@ -1,7 +1,19 @@
-"use strict"
+const resultado = document.querySelector(".resultado")
+const carga = document.querySelector(".cargar")
 
 
-nombre = 'ander';
+for (let i = 0; i <= 100; i++) {
+    setTimeout(() => {
+        resultado.textContent = `${i}%` 
+        carga.style.width = `${i}px`
+        if(i === 100){
+            carga.style.background = "green"
+            carga.style.transition = "0.5s"
+            resultado.style.transition = "0.5s"
+            resultado.textContent = "Cargar completa"
+        }
+    }, 50 * i);
+    
+}
 
 
-console.log(nombre)  
