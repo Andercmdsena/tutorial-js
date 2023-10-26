70fd3bf87a928b0817ea40d3edfb0c1ccde9abaf
Andercmdsena <andercmdsena>
2023-10-26 08:50:20 -0500

    hi

--- codigo.js
+++ codigo.js
@@ -1,19 +1,6 @@
-const resultado = document.querySelector(".resultado")
-const carga = document.querySelector(".cargar")
-
-
-for (let i = 0; i <= 100; i++) {
-    setTimeout(() => {
-        resultado.textContent = `${i}%` 
-        carga.style.width = `${i}px`
-        if(i === 100){
-            carga.style.background = "green"
-            carga.style.transition = "0.5s"
-            resultado.style.transition = "0.5s"
-            resultado.textContent = "Cargar completa"
-        }
-    }, 50 * i);
-    
-}
-
-
+if ("vibrate" in navigator) {
+    // Solicita una vibración de 1000 milisegundos (1 segundo).
+    navigator.vibrate(1000);
+  } else {
+    console.log("La API de Vibración no está disponible en este navegador.");
+  }
